c292a74ec9e52d710b281ace24be34845ebda75c
Jericho <jerichozara.tmlcr@gmail.com>
2026-07-16 12:41:40 +0800

    refactor: create reusable header and clean up global styles

---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+const ZaraFamilyExpenses = () => {
+    return (
+        <main className="mt-20 min-h-screen">
+            Zara Family Expenses
+        </main>
+    )
+}
+export default ZaraFamilyExpenses;
---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header
+            className="fixed top-0 w-full bg-[#5C4033] p-6"
+        >
+            <h1>Header</h1>
+        </header>
+    )
+}
+
+export default Header;
