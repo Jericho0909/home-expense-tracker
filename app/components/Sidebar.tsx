b5de5dfda2dd0ec46abe580ffc5e75abbba35b64
Jericho <jerichozara.tmlcr@gmail.com>
2026-07-17 12:48:16 +0800

    feat: implement clickAnywhere functionality for sidebar close behavior

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,13 +1,37 @@
+'use client'
+
+import { useRef, useEffect, useContext } from "react";
+import SidebarContext from '@/app/context/sidebarContext';
 import { motion } from "framer-motion";
 
 const Sidebar = () => {
+    const sidebarRef = useRef<HTMLDivElement | null>(null)
+    const { toggleSidebar } = useContext(SidebarContext)!
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (
+                sidebarRef.current &&
+                !sidebarRef.current.contains(event.target as Node)
+            ) {
+                toggleSidebar()
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
     return (
         <motion.aside
             initial={{ x: -300, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             exit={{ x: -300, opacity: 0 }}
             transition={{ duration: 0.3 }}
-            className="fixed top-20 left-0 h-full w-54 bg-[#8B5E3C] p-6"
+            ref={sidebarRef}
+            className="fixed top-0 left-0 h-full w-54 bg-[#8B5E3C] p-6"
         >
             <h2 className="text-xl font-bold">Expenses</h2>
         </motion.aside>
