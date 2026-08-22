d16a1442fcf5d9ac48877e9422aac274d88bda53
Jericho <jerichozara.tmlcr@gmail.com>
2026-08-22 09:35:08 +0800

    feat: add expense action modals and mappings

--- app/components/modal/Action.tsx
+++ app/components/modal/Action.tsx
@@ -7,16 +7,28 @@ import ContributionBudgetModal from "./ContributionBudgetModal"
 import EditMemberModal from "./EditMemberModal"
 import ViewMemberModal from "./ViewMemberModal"
 import UtilitiesModal from "./UtilitiesModal"
+import FoodAndHouseholdModal from "./FoodAndHouseholdModal";
+import TransportationModal from "./TransportationModal";
+import HealthModal from "./HealthModal";
+import HouseMaintenanceModal from "./HouseMaintenanceModal";
+import FamilyExpensesModal from "./FamilyExpensesModal";
+import OtherExepensesModal from "./OtherExpensesModal";
 import type { ActionContent } from "../../type/model"
  
 const ActionModal = ({action, id}: {action: ActionContent; id:string}) => { 
     const { setActiveModal } = useContext(ModalContext)! 
  
     const ActionContents: Record<ActionContent, ReactNode> = { 
-        editContribution: <ContributionBudgetModal id={id} />,
+        editContribution: <ContributionBudgetModal id={id}/>,
         editMember: <EditMemberModal id={id}/>,
         viewMember: <ViewMemberModal id={id}/>,
-        editUtilities: <UtilitiesModal id={id} />
+        editUtilities: <UtilitiesModal id={id} />,
+        editFoodAndHousehold: <FoodAndHouseholdModal id={id}/>,
+        editTransportation: <TransportationModal id={id}/>,
+        editHealth: <HealthModal id={id}/>,
+        editHouseMaintenance: <HouseMaintenanceModal id={id}/>,
+        editFamilyExpenses: <FamilyExpensesModal id={id}/>,
+        editOtherExpenses: <OtherExepensesModal id={id}/>
     } 
 
     useEffect(() => {
